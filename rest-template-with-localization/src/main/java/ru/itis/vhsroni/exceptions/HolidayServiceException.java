e4f95216df9accb6adfa3b7e70ae4bbc7147be77
VeronikaYamancheva <vshrony@mail.ru>
2025-04-22 08:00:50 +0300

    merge conflict refactor

--- rest-template-with-localization/src/main/java/ru/itis/vhsroni/exceptions/HolidayServiceException.java
+++
@@ -1,8 +0,0 @@
-package ru.itis.vhsroni.exceptions;
-
-public class HolidayServiceException extends ServiceException{
-
-    public HolidayServiceException(String errorMessage) {
-        super(500, errorMessage);
-    }
-}
